2353a38192cb92f0ce5d22ab0069af4c00468767
AbdullahMahiBD <AbdullahNT50@hotmail.com>
2026-08-05 22:31:17 +0600

    Logo Add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import DevOpsLogo from '../assets/devops-logo.svg';
+import DevOpsLogo from '../assets/devops-logo.png';
 
 const Header = () => {
   const today = new Date().toLocaleDateString('en-US', {
@@ -13,13 +13,13 @@ const Header = () => {
     <div className="hero">
       <div className="hero-content">
         <img src={DevOpsLogo} alt="DevOps" className="hero-image" />
-        <h1 className="hero-title">ADN Group</h1>
-        <p className="hero-subtitle">ADN Technologies Limited</p>
-        <p className="hero-subtitle-2">Technology Assessment & Adoption Framework| This my my night</p>
+        <h1 className="hero-title">Institute of Information Technology</h1>
+        <p className="hero-subtitle">Jahangirnagar University</p>
+        <p className="hero-subtitle-2">Savar, Dhaka-1342, Bangladesh.</p>
         <p className="hero-date">{today}</p>
       </div>
       <footer className="hero-footer">
-        &copy; {new Date().getFullYear()} IIT JU
+        &copy; {new Date().getFullYear()} IIT-JU Project. All rights reserved.
       </footer>
     </div>
   );
