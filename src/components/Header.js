b0e2ec33fd05dafafb7001c71e883aaf9ba8f7db
K M Abdulla Al Mamun <abdulla.mamun@adntechnologies.net>
2026-08-20 13:39:48 +0600

    trivy

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
         <img src={DevOpsLogo} alt="DevOps" className="hero-image" />
         <h1 className="hero-title">Institute of Information Technology</h1>
         <p className="hero-subtitle">Jahangirnagar University</p>
-        <p className="hero-subtitle-2">Savar, Dhaka-1342, Bangladesh. 18 August 2026, Today</p>
+        <p className="hero-subtitle-2">Savar, Dhaka-1342, Bangladesh. 18 August 2026</p>
         <p className="hero-date">{today}</p>
       </div>
       <footer className="hero-footer">
